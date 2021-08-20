0d704de55a4bce5c2ef3e67464fcb99657451572
peaceheis <navylsc4ever@gmail.com>
2021-08-20 17:22:17 +0000

    done?

--- factorfinder.py
+++ factorfinder.py
@@ -1,6 +1,6 @@
-import typing
+import math
 import sys
-
+#factor finder
 def factorial(num): 
     if num <= 1: 
         return 1
@@ -18,7 +18,7 @@ def get_possible_prime_factors(num):
     """Simple generator comprehension to return all the possible prime factors up to a number - Generators provide a much smaller
         memory footprint than lists."""
 
-    return (i for i in range(int(num/2)) if is_prime(i))
+    return (i for i in range(int(num/2)+1) if is_prime(i))
 
 def flatten(to_be_flattened):
     if len(to_be_flattened) == 1:
@@ -35,21 +35,38 @@ def flatten(to_be_flattened):
 def remove_duplicates(to_be_cleaned):
     returned_list = []
     for item in to_be_cleaned: 
-        if item not in to_be_cleaned: 
+        if item not in returned_list: 
             returned_list.append(item)
-        
+    return returned_list
+
 def find_factors(num, factors = []): 
+    if num == 1: 
+        return [1]
     if is_prime(num): 
         factors.append(num)
-        return remove_duplicates(factors)
+        return remove_duplicates([1, *factors, num])
     #time to connect everything
     primes = get_possible_prime_factors(num)
     for prime in primes: 
         if num % prime == 0: 
-            non_prime_factor = num/prime
-            factors.append([prime, non_prime_factor])
-            print(factors)
-            return remove_duplicates(factors + (flatten(find_factors(non_prime_factor, factors))))
-        
+            other_factor = int(num/prime)
+            factors.append([prime, other_factor])
+            return remove_duplicates([1, *flatten(factors + (find_factors(other_factor, factors))), num])
+    
 
-print(find_factors(20))
+#factor analyzing
+
+last = int(input("To what number would you like to analyze factors? "))
+sys.setrecursionlimit(last+1)
+
+analyzed_list = []
+for i in range(1, last+1): 
+    factors = find_factors(i, factors = []) 
+    analyzed_list.append((i, len(factors), round(len(factors)/i, ndigits=3))) #(number, number of factors, ratio of factors to all numbers below)
+
+analyzed_list = sorted(analyzed_list, key=lambda tup: tup[2], reverse=True)[:10] #sort in decreasing order by ratio
+
+print(f"Done! Here are the 10 numbers from 1 to {last} with the highest ratios of factors to numbers less than the number:")
+for i, analyzed in enumerate(analyzed_list): 
+    print(f"{i+1}: {analyzed[0]} with {analyzed[1]} factors, a ratio of {analyzed[2]}") #Ex: 1: 10 with 4 factors, a ratio of ...
+
