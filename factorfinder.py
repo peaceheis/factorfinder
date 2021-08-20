700712da9f027de34f79a12ce8b0365f406e9a53
peaceheis <navylsc4ever@gmail.com>
2021-08-20 01:05:39 +0000

    near finish

--- factorfinder.py
+++ factorfinder.py
@@ -1,4 +1,5 @@
-import math
+import typing
+import sys
 
 def factorial(num): 
     if num <= 1: 
@@ -8,22 +9,44 @@ def factorial(num):
 
 def is_prime(num):
     """According to Wilson's Theorem, a number is prime if and only if (n-1)! + 1 is divisible by n."""
-    return ((factorial(num - 1) + 1) % num == 0)
+    if num > 1: 
+        return ((factorial(num - 1) + 1) % num == 0)
+    else: 
+        return False
 
 def get_possible_prime_factors(num): 
     """Simple generator comprehension to return all the possible prime factors up to a number - Generators provide a much smaller
         memory footprint than lists."""
+
     return (i for i in range(int(num/2)) if is_prime(i))
 
-def find_factors(num): 
-    if is_prime(num): 
-        return num
+def flatten(to_be_flattened):
+    if len(to_be_flattened) == 1:
+            if type(to_be_flattened[0]) == list:
+                    result = flatten(to_be_flattened[0])   
+            else:
+                    result = to_be_flattened
+    elif type(to_be_flattened[0]) == list:
+            result = flatten(to_be_flattened[0]) + flatten(to_be_flattened[1:])
+    else:
+            result = [to_be_flattened[0]] + flatten(to_be_flattened[1:])
+    return result
 
+def remove_duplicates(to_be_cleaned):
+    return list(dict.fromkeys(to_be_cleaned))
+        
+def find_factors(num, factors = []): 
+    if is_prime(num): 
+        factors.append(num)
+        return remove_duplicates(factors)
     #time to connect everything
     primes = get_possible_prime_factors(num)
-    print(primes)
-    print(next(primes))
     for prime in primes: 
         if num % prime == 0: 
-            return set(prime, find_factors(num/prime))
+            non_prime_factor = num/prime
+            factors.append([prime, non_prime_factor])
+            print(factors)
+            return remove_duplicates(factors + (flatten(find_factors(non_prime_factor, factors))))
         
+
+print(find_factors(20))
